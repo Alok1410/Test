10f4d7591071e3393f40ab442a123341b9159787
Alok1410 <31375446+Alok1410@users.noreply.github.com>
2019-04-12 00:46:29 +0530

    Update alok.java

--- Demo/src/test/java/org/testng/com/Demo/alok.java
+++ Demo/src/test/java/org/testng/com/Demo/alok.java
@@ -7,7 +7,7 @@ public class alok
         @Test
         public void s()
             {
-                System.out.println("Alok Sharma 123");
+                System.out.println("Alok Sharma 23");
 
             }
 
